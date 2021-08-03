1cdb010255f0b26dba21312b0c8233189072193b
Predators <31931530+predators46@users.noreply.github.com>
2021-08-03 21:34:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,30 +107,6 @@ function install_libernet() {
     && sed -i "s/LIBERNET_DIR/$(echo ${LIBERNET_DIR} | sed 's/\//\\\//g')/g" "${LIBERNET_WWW}/config.inc.php"
 }
 
-function configure_libernet_firewall() {
-  if ! uci get network.libernet > /dev/null 2>&1; then
-    echo "Configuring Libernet firewall" \
-      && uci set network.libernet=interface \
-      && uci set network.libernet.proto='none' \
-      && uci set network.libernet.ifname='tun1' \
-      && uci commit \
-      && uci add firewall zone \
-      && uci set firewall.@zone[-1].network='libernet' \
-      && uci set firewall.@zone[-1].name='libernet' \
-      && uci set firewall.@zone[-1].masq='1' \
-      && uci set firewall.@zone[-1].mtu_fix='1' \
-      && uci set firewall.@zone[-1].input='REJECT' \
-      && uci set firewall.@zone[-1].forward='REJECT' \
-      && uci set firewall.@zone[-1].output='ACCEPT' \
-      && uci commit \
-      && uci add firewall forwarding \
-      && uci set firewall.@forwarding[-1].src='lan' \
-      && uci set firewall.@forwarding[-1].dest='libernet' \
-      && uci commit \
-      && /etc/init.d/network restart
-  fi
-}
-
 function configure_libernet_service() {
   echo -e "Configuring Libernet service"
   # disable dns resolver startup
@@ -147,7 +123,6 @@ function main_installer() {
     && install_libernet \
     && add_libernet_environment \
     && enable_uhttp_php \
-    && configure_libernet_firewall \
     && configure_libernet_service \
     && finish_install
 }
